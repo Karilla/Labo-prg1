7c6ddf0d52073b522ed82d8d4663a8a3f4fcd7be
karilla <bdelay155@gmail.com>
2021-12-08 12:59:14 +0100

    Added Labo 28

---
+++ Labo 28/delay_benoit_labo28.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file delay_benoit_labo28.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <iostream>
+#include <vector>
+#include "word.hpp"
+
+using namespace std;
+
+int main()
+{
+    vector<Word> w1;
+
+    w1.push_back(Word("Yo mon reuf"));
+
+    //cout << w1[0].getTextWord() << endl;
+}
