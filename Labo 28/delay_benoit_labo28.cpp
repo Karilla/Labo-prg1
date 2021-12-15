325436f2952bc939582fa11a797e684bd1761554
karilla <bdelay155@gmail.com>
2021-12-15 10:42:56 +0100

    youpilol

---
+++ Labo 28/delay_benoit_labo28.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file delay_benoit_labo28.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <iostream>
+#include <vector>
+#include <cctype>
+#include "word.hpp"
+
+using namespace std;
+
+
+
+<php dotz comlksdfbkads lidbfasdkhlbasd
+
+int main()
+{
+    string text = "CaCA";
+    vector<Word> w1;
+    Word w2();
+    //getline(cin, text, '\0');
+
+
+/*
+    for(int i = 0; i < text.size();i++)
+    {
+        if(!isalnum(text[i]) or ispunct(text[i]))
+        {
+            if(!sub.empty())
+            {
+                v1.push_back(sub);
+            }
+            sub = "";
+        }
+        else
+        {
+            sub.push_back(text[i]);
+        }
+    }
+*/
+}
