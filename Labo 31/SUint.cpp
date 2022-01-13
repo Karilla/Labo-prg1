a650aec8ea8787d73c612e0c278bf58321b993dd
karilla <bdelay155@gmail.com>
2022-01-13 02:30:59 +0100

    Added operator *

--- Labo 31/SUint.cpp
+++ Labo 31/SUint.cpp
@@ -4,10 +4,13 @@
 
 #include "SUint.h"
 
+using namespace std;
+
+const string ERROR_STRING_NUM = "ERROR TO CONVERT STRING TO SUINT, CHARACTER IS NOT A DIGIT AT POS ";
 
 SUint::SUint(int nb)
 {
-    if(number < 0)
+    if(nb < 0)
     {
         sign = true;
         number = (nb * -1);
@@ -21,15 +24,61 @@ SUint::SUint(int nb)
 
 SUint::SUint(std::string nb)
 {
-    if(nb.at(nb.size() - 1) == '-')
+    if(nb.at(0) == '-')
     {
-        sign = true;    
-        nb.pop_back();
-        number = nb;
+        sign = true;
+        nb.erase(nb.begin(), nb.begin() + 1);
     }
     else
     {
         sign = false;
-        number = nb;
     }
+    for(size_t i = 0; i < nb.size(); i++)
+    {
+        if(!isdigit(nb.at(i)))
+        {
+            cerr << ERROR_STRING_NUM << i + sign <<endl;
+            nb = "404";
+        }
+    }
+    number = nb;
+}
+
+SUint &SUint::operator*=(const SUint &rhs)
+{
+    this->number *= rhs.number;
+    this->sign = this->sign ^ rhs.sign;
+    return *this;
+}
+
+SUint operator*(SUint lhs, const int scalaire)
+{
+    SUint temp;
+    if(scalaire < 0)
+    {
+        temp.number = lhs.number * (scalaire * -1);
+        temp.sign = !lhs.sign;
+    }
+    else
+    {
+        temp.sign = lhs.sign;
+        temp.number = lhs.number * scalaire;
+    }
+
+
+    return temp;
+}
+
+std::ostream &operator<<(std::ostream &lhs, const SUint &rhs)
+{
+    if(rhs.sign)
+    {
+        lhs << "-" << rhs.number;
+    }
+    else
+    {
+        lhs << rhs.number;
+    }
+
+    return lhs;
 }
--- Labo 31/SUint.h
+++ Labo 31/SUint.h
@@ -6,14 +6,26 @@
 #define LABO_31_SUINT_H
 
 #include "Uint.h"
+#include <iostream>
+#include <cctype>
 
 class SUint {
+
+    friend std::ostream &operator<<(std::ostream &lhs, const SUint &rhs);
+
+    friend SUint operator*(SUint lhs, const Uint &rhs);
+
+    friend SUint operator*(SUint lhs, const int scalaire);
+
 public:
 
     SUint() = default;
 
     SUint(std::string number);
+
     SUint(int number);
+
+    SUint &operator*=(const SUint &rhs);
 
 private:
     Uint number;
