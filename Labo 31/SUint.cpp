3eead71b0409ee21f8b8bb7583ed47dff726442f
karilla <bdelay155@gmail.com>
2022-01-13 01:16:19 +0100

    Added SUint class and constructor

---
+++ Labo 31/SUint.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bdela on 13/01/2022.
+//
+
+#include "SUint.h"
---
+++ Labo 31/SUint.h
@@ -0,0 +1,14 @@
+//
+// Created by bdela on 13/01/2022.
+//
+
+#ifndef LABO_31_SUINT_H
+#define LABO_31_SUINT_H
+
+
+class SUint {
+
+};
+
+
+#endif //LABO_31_SUINT_H
